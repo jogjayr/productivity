b9e5fc01ef3c667b0dfbc55701b19bdf76778fb1
Jayraj Jog <jogjayr@gmail.com>
2015-05-17 22:37:48 -0700

    Add onboarding page to select some bad sites

--- chrome/app/scripts/background.js
+++ chrome/app/scripts/background.js
@@ -1,28 +1,10 @@
 'use strict';
 
-var EXTENSION_ID = chrome.runtime.id;
-var forbiddenHosts = ['facebook.com', 'twitter.com', 'pinterest.com', 'quora.com'];
+// var EXTENSION_ID = chrome.runtime.id;
+// var forbiddenHosts = ['facebook.com', 'twitter.com', 'pinterest.com', 'quora.com'];
 
-function buildRules(hosts) {
-    return hosts.map(function(host) {
-        return {
-            hostSuffix: host
-        };
-    });
-}
 
 chrome.runtime.onInstalled.addListener(function(details) {
-    var rules = buildRules(forbiddenHosts);
-    chrome.webNavigation.onBeforeNavigate.addListener(function(e) {
-        //ensures no nav takes place if forbidden iframe exists
-        if (e.parentFrameId !== 0) {
-            console.log(e);
-            chrome.tabs.update(e.tabId, {url: '/alternatives.html'});
-        }
-    }, {
-        url: rules
-    });
-
     chrome.tabs.create({
         url: '/options.html'
     })
--- chrome/app/scripts/options.js
+++ chrome/app/scripts/options.js
@@ -2,13 +2,44 @@
 
 var LazyHacker = angular.module('LazyHacker', {});
 
-LazyHacker.controller('OnboardController', function ($scope) {
+
+
+LazyHacker.controller('OnboardController', function($scope) {
     $scope.banned = [{
         name: 'Facebook',
         hostname: 'facebook.com'
     }, {
         name: 'Twitter',
         hostname: 'twitter.com'
+    }, {
+        name: 'Tumblr',
+        hostname: 'tumblr.com'
     }];
 
+    $scope.saveBanned = function() {
+        var rules = buildRules(this.banned.filter(function(site) {
+            return site.checked;
+        }));
+        chrome.webNavigation.onBeforeNavigate.addListener(function(e) {
+            //ensures no nav takes place if forbidden iframe exists
+            if (e.parentFrameId !== 0) {
+                console.log(e);
+                chrome.tabs.update(e.tabId, {
+                    url: '/alternatives.html'
+                });
+            }
+        }, {
+            url: rules
+        });
+    };
+
+
+
+    function buildRules(hosts) {
+        return hosts.map(function(host) {
+            return {
+                hostSuffix: host.hostname
+            };
+        });
+    }
 });
