8d08cda07ab0fc5c38a1a3e1e4855f462f3aefa9
Jayraj Jog <jogjayr@gmail.com>
2015-05-17 17:13:16 -0700

    Console logs when you go to some forbidden websites

--- chrome/app/scripts/background.js
+++ chrome/app/scripts/background.js
@@ -1,11 +1,27 @@
 'use strict';
+var timesCalled = 0;
 
-chrome.runtime.onInstalled.addListener(function (details) {
-  console.log('previousVersion', details.previousVersion);
+var forbiddenHosts = ['facebook.com', 'twitter.com', 'pinterest.com', 'quora.com'];
+
+function buildRules(hosts) {
+    return hosts.map(function(host) {
+        return {
+            hostSuffix: host
+        };
+    });
+}
+
+chrome.runtime.onInstalled.addListener(function(details) {
+    console.log('previousVersion', details.previousVersion);
+    var rules = buildRules(forbiddenHosts);
+    chrome.webNavigation.onBeforeNavigate.addListener(function(e) {
+        console.log('You just went to Google' + timesCalled);
+        timesCalled++;
+    }, {
+        url: rules
+    });
 });
 
-chrome.tabs.onUpdated.addListener(function (tabId) {
-  chrome.pageAction.show(tabId);
-});
-
-console.log('\'Allo \'Allo! Event Page for Page Action');
+// chrome.tabs.onUpdated.addListener(function (tabId) {
+//   chrome.pageAction.show(tabId);
+// });
