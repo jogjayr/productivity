8a90fb01c971b4c4a3aac0423e6e1c6da8e91598
Jayraj Jog <jogjayr@gmail.com>
2015-05-20 10:04:57 -0700

    Close onboarding page once done onboarding

--- chrome/app/scripts/select-sources.js
+++ chrome/app/scripts/select-sources.js
@@ -1,9 +1,10 @@
 LazyHacker.controller('SourcesController', ['$scope', 'GithubService', function($scope, GithubService) {
+    $scope.showThanks = false;
     $scope.handleGithubSubmit = function() {
         localStorage.setItem('githubUsername', this.githubUsername);
-        GithubService.getGithubStarred(this.githubUsername).success(function(data) {
-            console.log(data);
-            $scope.githubStarred = data;
-        });
+        $scope.showThanks = true;
+        setTimeout(function() {
+            window.close();
+        }, 1000);
     };
 }]);
