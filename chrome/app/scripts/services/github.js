db53724635a5957a7aee981c80186c46a8d32c9f
Jayraj Jog <jogjayr@gmail.com>
2015-05-24 14:19:20 -0700

    Add cache: true. Doesn't always work though :/

--- chrome/app/scripts/services/github.js
+++ chrome/app/scripts/services/github.js
@@ -1,6 +1,6 @@
 'use strict';
 LazyHacker.service('GithubService', ['$http', function($http) {
     this.getGithubStarred = function(username) {
-        return $http.get('https://api.github.com/users/' + username + '/starred');
+        return $http.get('https://api.github.com/users/' + username + '/starred', {cache: true});
     };
 }]);
